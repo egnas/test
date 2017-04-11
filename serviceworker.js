61630239b62b8d1af0b45e2cd3f0566b5ba07266
egnas <m019986e@student.staffs.ac.uk>
2017-04-11 17:07:36 +0100

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -3,45 +3,45 @@ var CACHE_NAME = 'gih-cache-v6';
 var CACHED_URLS = [
     // Our HTML
     BASE_PATH + 'first.html',
-    
+
     // Images for favicons
-    BASE_PATH + 'appimages/android-icon-36x36.png',
-    BASE_PATH + 'appimages/android-icon-48x48.png',
-    BASE_PATH + 'appimages/android-icon-72x72.png',
-    BASE_PATH + 'appimages/android-icon-96x96.png',
-    BASE_PATH + 'appimages/android-icon-144x144.png',
-    BASE_PATH + 'appimages/android-icon-192x192.png',
-    BASE_PATH + 'appimages/favicon-32x32.png',
+    BASE_PATH + 'Task5and6/images/android-logo36x36.png',
+    BASE_PATH + 'Task5and6/images/android-logo48x48.png',
+    BASE_PATH + 'Task5and6/images/android-logo72x72.png',
+    BASE_PATH + 'Task5and6/images/android-logo96x96.png',
+    BASE_PATH + 'Task5and6/images/android-logo144x144.png',
+    BASE_PATH + 'Task5and6/images/android-logo192x192.png',
+    BASE_PATH + 'Task5and6/images/fav-logo32x32.png',
 
     //Images for page
-    BASE_PATH + 'appimages/offlinemap.jpg',
-    BASE_PATH + 'appimages/dino.png',
-    BASE_PATH + 'appimages/jack.jpg',
-    BASE_PATH + 'appimages/paddy.jpg',
-    BASE_PATH + 'appimages/favicon.ico',
-    BASE_PATH + 'appimages/favicon-16x16.png',
-    BASE_PATH + 'appimages/favicon-32x32.png',
-    BASE_PATH + 'appimages/favicon-96x96.png',
-    BASE_PATH + 'appimages/ms-icon-70x70.png',
-    BASE_PATH + 'appimages/ms-icon-144x144.png',
-    BASE_PATH + 'appimages/ms-icon-150x150.png',
-    BASE_PATH + 'appimages/ms-icon-310x310.png',
-    BASE_PATH + 'eventsimages/example-blog01.jpg',
-    BASE_PATH + 'eventsimages/example-blog02.jpg',
-    BASE_PATH + 'eventsimages/example-blog03.jpg',
-    BASE_PATH + 'eventsimages/example-blog04.jpg',
-    BASE_PATH + 'eventsimages/example-blog05.jpg',
-    BASE_PATH + 'eventsimages/example-blog06.jpg',
-    BASE_PATH + 'eventsimages/example-blog07.jpg',
-    BASE_PATH + 'eventsimages/example-work01.jpg',
-    BASE_PATH + 'eventsimages/example-work02.jpg',
-    BASE_PATH + 'eventsimages/example-work03.jpg',
-    BASE_PATH + 'eventsimages/example-work04.jpg',
-    BASE_PATH + 'eventsimages/example-work05.jpg',
-    BASE_PATH + 'eventsimages/example-work06.jpg',
-    BASE_PATH + 'eventsimages/example-work07.jpg',
-    BASE_PATH + 'eventsimages/example-work08.jpg',
-    BASE_PATH + 'eventsimages/example-work09.jpg',  
+    BASE_PATH + 'Task5and6/images/offlinemap.jpg',
+    BASE_PATH + 'Task5and6/images/books.png',
+    // BASE_PATH + 'Task5and6/images/jack.jpg',
+    // BASE_PATH + 'Task5and6/images/paddy.jpg',
+    BASE_PATH + 'Task5and6/images/fav-logo.ico',
+    BASE_PATH + 'Task5and6/images/fav-logo16x16.png',
+    BASE_PATH + 'Task5and6/images/fav-logo32x32.png',
+    BASE_PATH + 'Task5and6/images/fav-logo96x96.png',
+    BASE_PATH + 'Task5and6/images/ms-logo70x70.png',
+    BASE_PATH + 'Task5and6/images/ms-logo144x144.png',
+    BASE_PATH + 'Task5and6/images/ms-logo150x150.png',
+    BASE_PATH + 'Task5and6/images/ms-logo310x310.png',
+    // BASE_PATH + 'eventsimages/example-blog01.jpg',
+    // BASE_PATH + 'eventsimages/example-blog02.jpg',
+    // BASE_PATH + 'eventsimages/example-blog03.jpg',
+    // BASE_PATH + 'eventsimages/example-blog04.jpg',
+    // BASE_PATH + 'eventsimages/example-blog05.jpg',
+    // BASE_PATH + 'eventsimages/example-blog06.jpg',
+    // BASE_PATH + 'eventsimages/example-blog07.jpg',
+    // BASE_PATH + 'eventsimages/example-work01.jpg',
+    // BASE_PATH + 'eventsimages/example-work02.jpg',
+    // BASE_PATH + 'eventsimages/example-work03.jpg',
+    // BASE_PATH + 'eventsimages/example-work04.jpg',
+    // BASE_PATH + 'eventsimages/example-work05.jpg',
+    // BASE_PATH + 'eventsimages/example-work06.jpg',
+    // BASE_PATH + 'eventsimages/example-work07.jpg',
+    // BASE_PATH + 'eventsimages/example-work08.jpg',
+    // BASE_PATH + 'eventsimages/example-work09.jpg',
     // JavaScript
     BASE_PATH + 'offline-map.js',
     BASE_PATH + 'material.js',
@@ -149,8 +149,3 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
-
-
-
-
-
